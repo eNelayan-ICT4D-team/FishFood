677032da47949c8a46eff8260e1306d29353ef39
Adrian S.A <44787687+maplesyrup18@users.noreply.github.com>
2022-06-22 01:06:23 +0200

    purchasescreen component and fixed search bug

---
+++ src/app/user-purchase-confirmation/user-purchase-confirmation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-purchase-confirmation',
+  templateUrl: './user-purchase-confirmation.component.html',
+  styleUrls: ['./user-purchase-confirmation.component.scss']
+})
+export class UserPurchaseConfirmationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
